25549f09c672dab3df716391804268fb127f8974
juusotaneli <juuso.karvonen@outlook.com>
2020-02-13 23:03:48 +0200

    task 4.22

--- Viikko4/tests/blogs_api.test.js
+++ Viikko4/tests/blogs_api.test.js
@@ -4,6 +4,9 @@ const app = require('../app')
 const helper = require('./test_helper')
 const api = supertest(app)
 const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
 
 beforeEach(async () => {
@@ -30,25 +33,96 @@ test('id field doesnt contain "_"', async () => {
 
 })
 test('blogs can be added', async () => {
+  await User.deleteMany({})
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash("salasana", saltRounds)
+
+  let userObject = new User({
+    username: "esa",
+    name: "Esa Esanen",
+    passwordHash
+  })
+  let a = await userObject.save()
+
+  console.log(a)
+  let token
+  await api
+    .post('/api/login')
+    .send({
+      username: "esa",
+      password: "salasana"
+    })
+    .then(res => token = res.body.token)
+
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+
+  user = await User.findById(decodedToken.id)
+
   const newBlog =
   {
     title: "Kukkienhoito",
     author: "S. Eppo",
     url: "www.kukkamies.fi",
-    likes: 0
+    likes: 0,
+    user: user
   }
+
   await api
     .post('/api/blogs')
+    .set('Authorization', 'Bearer ' + token)
     .send(newBlog)
     .expect(200)
 
   const b = await helper.blogsInDb()
   expect(b.length).toBe(helper.initialBlogs.length + 1)
   expect(b[2].title).toContain("hoito")
-
 })
-afterAll(() => {
-  mongoose.connection.close()
+test('blogs cant be added with invalid token', async () => {
+  await User.deleteMany({})
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash("salasana", saltRounds)
+
+  let userObject = new User({
+    username: "esa",
+    name: "Esa Esanen",
+    passwordHash
+  })
+  let a = await userObject.save()
+
+  console.log(a)
+  let token
+  await api
+    .post('/api/login')
+    .send({
+      username: "esa",
+      password: "salasana"
+    })
+    .then(res => token = res.body.token)
+
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+
+  user = await User.findById(decodedToken.id)
+
+  const newBlog =
+  {
+    title: "Kukkienhoito",
+    author: "S. Eppo",
+    url: "www.kukkamies.fi",
+    likes: 0,
+    user: user
+  }
+
+  await api
+    .post('/api/blogs')
+    .set('Authorization', 'Beare ' + token)
+    .send(newBlog)
+    .expect(401)
+
+  const b = await helper.blogsInDb()
+  expect(b.length).toBe(helper.initialBlogs.length)
+
 })
 test('if likes is null amount of likes is set to zero', async () => {
   const newBlog =
@@ -60,7 +134,7 @@ test('if likes is null amount of likes is set to zero', async () => {
   await api
     .post('/api/blogs')
     .send(newBlog)
-    .expect(200)
+    .expect(401)
 
   const b = await helper.blogsInDb()
   expect(b[2].likes).toBe(0)
@@ -107,9 +181,12 @@ test('blogs can be updated', async () => {
     .put(`/api/blogs/${blogToUpdate.id}`)
     .send(blogToUpdate)
     .expect(204)
-  
+
   const blogsInTheEnd = await helper.blogsInDb()
 
   expect(blogsInTheEnd[0].likes).toBe(57)
-
 })
+afterAll(() => {
+  mongoose.connection.close()
+})
+

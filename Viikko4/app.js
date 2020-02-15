3bc499c3c5352a965c51d9dbf0126494fe1b1652
juusotaneli <juuso.karvonen@outlook.com>
2020-02-15 11:22:10 +0200

    task 5.1

--- Viikko4/app.js
+++ Viikko4/app.js
@@ -1,16 +1,14 @@
 require('express-async-errors')
+const cors = require('cors')
 const config = require('./utils/config')
 const express = require('express')
 const app = express()
-const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
-
-
 
 
 logger.info('connecting to', config.MONGODB_URI)
@@ -22,7 +20,6 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
   .catch((error) => {
     logger.error('error connection to MongoDB:', error.message)
   })
-
 
 app.use(cors())
 app.use(express.static('build'))
